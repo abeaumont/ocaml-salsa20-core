6b54c7e74f1d17e0cfbf34b0036d47b7d37aaac8
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-08-14 16:30:00 +0200

    Add Utils module to the mllib file, without public API

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -12,7 +12,7 @@ let () =
   Pkg.describe "salsa20-core" ~build @@ fun _c ->
   Ok [
     Pkg.clib "libsalsa-core.clib";
-    Pkg.mllib "salsa20-core.mllib";
+    Pkg.mllib ~api:["Salsa20_core"] "salsa20-core.mllib";
     Pkg.test "salsa20_core_tests";
     Pkg.test "utils_tests"
   ]
